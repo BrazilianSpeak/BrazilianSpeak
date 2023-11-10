4e8618f89b285471f7202fed954546c3ffdf5690
Hiago Briano <hiago.artist@hotmail.com>
2023-11-10 17:19:51 -0300

    rota de login do professor e aluno criada

--- backend/src/routers/routerLogin.ts
+++ backend/src/routers/routerLogin.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import validateEmail from '../middlewares/validateEmail';
 import validatePassword from '../middlewares/validatePassword';
+import { controllerLoginStudent, controllerLoginTeacher } from '../controller/controllerLogin';
 
 const routerLogin = Router();
 
-routerLogin.route('/').post(validateEmail, validatePassword);
+routerLogin.route('/student').post(validateEmail, validatePassword, controllerLoginStudent);
+routerLogin.route('/teacher').post(validateEmail, validatePassword, controllerLoginTeacher);
 
 export default routerLogin;
