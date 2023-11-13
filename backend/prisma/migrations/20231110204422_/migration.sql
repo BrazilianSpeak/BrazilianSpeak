a136b88eed1278b88cbaf2c22c5828c5b909ab76
Hiago Briano <hiago.artist@hotmail.com>
2023-11-12 22:58:09 -0300

    sala da reunião adicionada ao banco de dados

--- backend/prisma/migrations/20231110204422_/migration.sql
+++
@@ -1,7 +0,0 @@
--- AlterTable
-ALTER TABLE "students" ALTER COLUMN "lastLogin" DROP NOT NULL,
-ALTER COLUMN "updatedAt" DROP NOT NULL;
-
--- AlterTable
-ALTER TABLE "teachers" ALTER COLUMN "lastLogin" DROP NOT NULL,
-ALTER COLUMN "updatedAt" DROP NOT NULL;
