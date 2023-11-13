00af0fcd81618245030a5c87e37bfea82df41ad6
Hiago Briano <hiago.artist@hotmail.com>
2023-11-12 23:36:48 -0300

    documentacao criada com swagger

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,6 +3,8 @@ import express from 'express';
 import routerLogin from './routers/routerLogin';
 import serverError from './middlewares/serverError';
 import routerCreate from './routers/routerCreate';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './swagger.json';
 require('dotenv/config');
 
 const app = express();
@@ -13,6 +15,7 @@ const PORT = process.env.PORT || 3001;
 
 app.use('/create', routerCreate);
 app.use('/login', routerLogin);
+app.use('/documentation', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.get('/', (_req, res) => res.json({ message: 'active server' }));
 app.use(serverError);
 
